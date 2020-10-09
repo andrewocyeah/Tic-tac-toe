aa5963c47a093357eaa96b8dcc1ee11de06d4492
ANDREW RICE <andrewr146@nycstudents.net>
2020-10-09 21:44:57 +0000

    Added preamble(Description+ starting variables)

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-print("Version 1.0.0 - Last Commit: Andrew Rice")
-print("Original developer: Andrew Rice")
+print("Version 1.0.0 - Last Commit: Andrew Rice\n")
+print("Original developer: Andrew Rice\n\n")
 
+print("This project takes advantage of the fact, \n")
+print("That multiple people can use the Console\n\n")
+
+print("All text inputs are suffixed by (User 1)|(User 2)\n")
+
+print("This is to specify for who the input is for\n\n")
+
+input("May we begin, Press Enter(Any User)")
+
+print("\n")
+
+tic_tac_toe_board = [['','',''],['','',''],['','','']]
+
+running = True
+
+while running:
+    
