a0ad5b366cee437c9080c199fb4c3773b846426f
ANDREW RICE <andrewr146@nycstudents.net>
2020-10-11 01:34:18 +0000

    Added tying Game is playable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-#Andrew Rice : Original developer 
-
-
+#Andrew Rice : Original developer
 
 #Preamble
 print("Version 1.0.1 - Last Commit: Andrew Rice\n")
@@ -17,81 +15,108 @@ input("May we begin, Press Enter(Any User)")
 
 print("\n")
 
-tic_tac_toe_board = ['---','---','---']#the board itself
+tic_tac_toe_board = ['---', '---', '---']  #the board itself
 
-running = True#making this false makes the code end, bc the main loop runs on this
+running = True  #making this false makes the code end, bc the main loop runs on this
 
-def display_board():#Print Each row of the board
+
+def display_board():  #Print Each row of the board
     print(tic_tac_toe_board[0])
     print(tic_tac_toe_board[1])
     print(tic_tac_toe_board[2])
 
-display_board();#Show board for reference
+
+display_board()
+#Show board for reference
+
 
 def check_victory(player_letter):
-    for i in range(3):#for each row
-        if tic_tac_toe_board[i] == player_letter*3 : return True #check for a horizontal row
+    for i in range(3):  #for each row
+        if tic_tac_toe_board[i] == player_letter * 3:
+            return True  #check for a horizontal row
         #horizontal rows can 1:be tested in a single line, 2. simpler (IDE was complaining about program complexity)
-    
-    for i in range(3):#for each collum
-        valid = True#anomly check
-        for j in range(3): #for each row
-            if tic_tac_toe_board[j][i] != player_letter : #if not letter collum win is not true
+
+    for i in range(3):  #for each collum
+        valid = True  #anomly check
+        for j in range(3):  #for each row
+            if tic_tac_toe_board[j][
+                    i] != player_letter:  #if not letter collum win is not true
                 valid = False
                 break
-        if valid:return True#detect win
+        if valid: return True  #detect win
 
     valid = True
     for i in range(3):
-        if tic_tac_toe_board[i][i] != player_letter : valid = False#scan for 0,0 - 2,2 diagonal win
-    if valid:return True
-    
-    valid = True   
+        if tic_tac_toe_board[i][i] != player_letter:
+            valid = False  #scan for 0,0 - 2,2 diagonal win
+    if valid: return True
+
+    valid = True
     for i in range(3):
-        if tic_tac_toe_board[i][2-i] != player_letter : valid = False#scan for 0,2 - 2,0 diagonal win
+        if tic_tac_toe_board[i][2 - i] != player_letter:
+            valid = False  #scan for 0,2 - 2,0 diagonal win
     if valid:
         return True
-    return False#no wins detected
+    return False  #no wins detected
 
-def turn(player_letter,user_number):
+
+def test_tie():
+    for i in range(3):
+        for j in range(3):
+            if tic_tac_toe_board[i][j] == '-': return False  #Any empty squares
+    return True
+
+
+def turn(player_letter, user_number):
     xcord = 0
     ycord = 0
     invalid = True
     while invalid:
         while True:
-            xcord = int(input("X pos of "+player_letter+"? [input 0, 1 or 2] (User "+user_number+")"))
-            if -1 < xcord < 3:#in range?
-                break;
-            print("Thats invalid! Must be a number between 0 and 2")#do again
+            xcord = int(
+                input("X pos of " + player_letter +
+                      "? [input 0, 1 or 2] (User " + str(user_number) + ")"))
+            if -1 < xcord < 3:  #in range?
+                break
+            print("Thats invalid! Must be a number between 0 and 2")  #do again
         while True:
-            ycord = int(input("Y pos of "+player_letter+"? [input 0, 1 or 2] (User "+user_number+")"))
-            if -1 < ycord < 3:#in range
+            ycord = int(
+                input("Y pos of " + player_letter +
+                      "? [input 0, 1 or 2] (User " + str(user_number) + ")"))
+            if -1 < ycord < 3:  #in range
                 if tic_tac_toe_board[ycord][xcord] == '-':
-                    invalid = False#All is well Proceed to Assignment
-                    break;
+                    invalid = False  #All is well Proceed to Assignment
+                    break
                 else:
-                    print('That space is already taken, choose again')#invalid is True Loop back to beginning
-    newRow = '' #Strings are immutable have to make new string    
-    for i  in range(0,3):#index \es 0, 1, and 2
+                    print('That space is already taken, choose again')  #invalid is True Loop back to beginning
+                    break;
+    new_row = ''  #Strings are immutable have to make new string
+    for i in range(0, 3):  #index \es 0, 1, and 2
+        
         if xcord == i:
-            newRow += player_letter#Make a new string with X at the end
+            new_row += player_letter  #Make a new string with X at the end
+            
         else:
-            newRow += tic_tac_toe_board[ycord][xcord];#copy reference form original
-        tic_tac_toe_board[ycord] = newRow
+            new_row += tic_tac_toe_board[ycord][i]
+            #copy reference form original
+    tic_tac_toe_board[ycord] = new_row
     display_board()
-    if check_victory():return True
+    if check_victory(player_letter): return True
     return False
 
+
 while running:
-    if turn('X',1):
+    if turn('X', 1):  #Player 1 win?
         print("User 1 Victory")
-        running = True
-    else:
-        if turn('Y',2):
-            print("User 3 Victory")
-
-
-
-    
-
-
+        running = False
+    elif test_tie():  #not win? test for a tie
+        print("Tie Neither Player wins")
+        running = False
+    else:  #not win or tie? Player 2's turn
+        if turn('O', 2):  #player 2's turn, did he win?
+            print("User 2 Victory")
+            running = False
+        elif test_tie():  #not win? is it a tie
+            print("Tie Neither Player wins")
+            running = False
+#if not player 2 win or tie, back to player 1
